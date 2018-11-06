852c34de3d350ccdb9baa6b9a479ad89873495a4
Michelle Gouw <m.gouw999@gmail.com>
2018-11-06 11:53:39 -0800

    Update in-class

--- day-09/js/demo.js
+++ day-09/js/demo.js
@@ -1,1 +1,49 @@
 //js goes here
+
+let h1 = document.querySelector('h1');
+	h1.addEventListener('click', function() {
+	h1.style.fontSize = "5em";
+	h1.style.color = "blue";
+})
+
+let button  = document.querySelector('button');
+button.addEventListener('click', function() {
+	let message = "You clicked " + event.offsetX + " and " + event.offsetY;
+	let clickedElement = event.target;
+
+	console.log(message);
+	console.log(event.target);
+})
+
+let stylesheet = document.querySelector('[href*="css/none.css"]');
+console.log(stylesheet);
+
+function doSomething(item) {
+	document.querySelector(`#${item}`).addEventListener('click',
+		function() {
+			stylesheet.setAttribute('href', `css/${item}.css`);
+		});
+}
+
+let listItems = document.querySelectorAll(".styles li");
+for(var i in Object.keys(listItems)) doSomething(listItems[i].id);
+
+document.addEventListener('keydown', function(event){
+	if(event.key === 'ArrowUp') {
+		document.querySelector('h2').style.fontSize = "5em";
+	} else if (event.key === 'ArrowDown') {
+		document.querySelector('h2').style.fontSize = "1em";
+	}
+})
+
+let clickCount = 0;  //keep track of the "state" (global)
+document.querySelector('button').addEventListener('click', function() {
+    if(clickCount > 10) {  //decide what to do
+        console.log("I think you've had enough");
+    }
+    else {
+        clickCount++;  //change state (+1)
+        console.log('You clicked me!');
+    }
+});
+
--- day-12/js/index.js
+++ day-12/js/index.js
@@ -4,11 +4,24 @@
 // Class Puppy
 class Puppy {
     // Constructor: takes in an img url, and a sound
-
+    constructor(img="../imgs/complete.png", sound="WOOF!") {
+        this.img = img;
+        this.sound = sound;
+    }
     // Method for "speaking" using responsiveVoice
-
+    bark() {
+        responsiveVoice.speak(this.sound);
+    }
     // Render a Div that you can click on to bark
-
+    render() {
+        let puppyCard = $('<div>');
+        puppyCard.attr('title', `puppy that says ${this.sound}`);
+        puppyCard.attr('class', 'puppyCard col-sm-4');
+        puppyCard.attr('background-image', `url(${this.img})`);
+        puppyCard.append(`<img src=${this.img}` class='puppyCard/img');
+        puppyCard.click( () => this.bark() );
+        return puppyCard;
+    }
 }
 
 
@@ -52,23 +65,46 @@ class PuppyForm {
 // Class for the app
 class PuppyApp {
     // Constructor: takes in a parent element and list of puppies
-
+    constructor(parentElement, puppyList) {
+        this.parentElement = parentElement;
+        this.puppyList = puppyList;
+    }
     // Add puppy: pushes new data into list of puppies and re-renders the app
-
+    addPuppy(img, sound) {
+        console.log(this);
+        this.puppyList.push({
+            url: img, 
+            sound: sound
+        });
+        this.render();
+    }
 
     // Render;
     render() {
         // Empty parent element
-
+        this.parentElement.empty();
 
         // Create and render a new form
+        let form = new PuppyForm(this.addPuppy.bind(this));
+        this.parentElement.append(form.render());
 
         // Append puppy list element to parent (in a wrapper div)
-
+        let puppyWrapper = $('<div class="row">');
+        this.parentElement.append(puppyWrapper);
+        this.puppyList.map( (puppyInfo) => {
+            let newPuppy = new Puppy(puppyInfo.url, puppyInfo.sound);
+            puppyWrapper.append(newPuppy.render());
+        });
     }
 }
 
 // Create a new app with a single puppy
-
+let app = new PuppyApp(
+    $('#content'), 
+    [{
+        url: "../imgs/complete.png",
+        bark: "WOOF!"
+    }]
+)
 
 // Render the app
